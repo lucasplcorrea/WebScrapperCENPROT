69e5deb612a280fb50e4f23be56d97393195d5e6
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2023-11-30 16:37:33 -0300

    Add files via upload
    Correção do manifesto para versão 3.

--- extensao/popup.js
+++ extensao/popup.js
@@ -1,10 +1,10 @@
-document.getElementById('run-script').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var url = tabs[0].url;
-      if (url === 'https://cartorio.cenprotnacional.org.br/home/pedidos/certidao#') {
-          chrome.tabs.executeScript(tabs[0].id, {file: 'script.js'});
-      } else {
-          alert('Este script só pode ser executado na URL especificada.');
-      }
-  });
+document.getElementById('run-script').addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var url = tabs[0].url;
+        if (url === 'https://cartorio.cenprotnacional.org.br/home/pedidos/certidao#') {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'executeScript' });
+        } else {
+            alert('Este script só pode ser executado na URL especificada.');
+        }
+    });
 });
